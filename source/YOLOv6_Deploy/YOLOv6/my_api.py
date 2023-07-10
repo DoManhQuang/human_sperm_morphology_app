b314c81a34f3873ae27d68c5fbec991aa3d8329a
Do Manh Quang <quangdm2@ghtk.internal>
2023-07-10 18:55:25 +0700

    update my_api

--- source/YOLOv6_Deploy/YOLOv6/my_api.py
+++ source/YOLOv6_Deploy/YOLOv6/my_api.py
@@ -10,14 +10,15 @@ import my_yolov6
 
 # load model
 
-yolov6_model = my_yolov6.my_yolov6("./source/YOLOv6_Deploy/YOLOv6/weights/1.0/yolov6t_segrelu.pt", "cpu", 
-                                   "./source/YOLOv6_Deploy/YOLOv6/weights/miamia-sperm.yaml", 640, False)
+yolov6_model = my_yolov6.my_yolov6(r"J:\HaUI\nckh\human_sperm_morphology_app\source\YOLOv6_Deploy\YOLOv6\weights\1.0\yolov6t_segrelu.pt", "cpu",
+                                   r"J:\HaUI\nckh\human_sperm_morphology_app\source\YOLOv6_Deploy\YOLOv6\weights\miamia-sperm.yaml", 640, False)
 
-sperm_cls = tf.keras.models.load_model('./source/sperm_classification/model/smids_mobiv2.h5')
+sperm_cls = tf.keras.models.load_model(r'J:\HaUI\nckh\human_sperm_morphology_app\source\sperm_classification\model\smids_mobiv2.h5')
 
 # Khởi tạo Flask Server Backend
 app = Flask(__name__)
 api = Api(app)
+
 
 class HelloWorld(Resource):
     def get(self):
@@ -25,7 +26,7 @@ class HelloWorld(Resource):
 
 
 class PredictYoloV6(Resource):
-    def post():
+    def post(self):
         data = request.get_json()
         img_in = data["data"]
         mode = data["mode"]
@@ -33,13 +34,23 @@ class PredictYoloV6(Resource):
             # Nhận diên qua model Yolov6
             _len, img_out, imgs_crop = yolov6_model.infer(img_in)
             return{
-                "num_obj": _len,
+                "img_num_obj": _len,
                 "img_out": img_out,
                 "imgs_crop": imgs_crop
             }
         elif mode == "video":
+            video_data=[]
+            video_num_obj=[]
+            video_crop=[]
+            for img in img_in:
+                _len, img_out, imgs_crop = yolov6_model.infer(img)
+                video_data.append(img_out)
+                video_num_obj.append(_len)
+                video_crop.append(imgs_crop)
             return {
-                "message": "chua ho tro"
+                "video_num_obj": video_num_obj,
+                "video_out": video_data,
+                "video_crop": video_crop
             }
         return{
             "message": "mode is image or video"
@@ -47,13 +58,14 @@ class PredictYoloV6(Resource):
 
 
 class CNNPredictSperm(Resource):
-    def post():
+    def post(self):
         data = request.get_json()
         img_in = data["data"]
         results = sperm_cls.predict(img_in)
         return{
             "results": results
         }
+
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(PredictYoloV6, '/api/yolo/v6/predict')
@@ -63,7 +75,7 @@ api.add_resource(CNNPredictSperm, '/api/cnn/cls/predict')
 # Start Backend
 if __name__ == '__main__':
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-port", type=str, default="6868", help="port")
+    parser.add_argument("-port", type=str, default="6969", help="port")
     args = vars(parser.parse_args())
     app_port = args["port"]
     app.run(debug=True, host='0.0.0.0', port=app_port)
